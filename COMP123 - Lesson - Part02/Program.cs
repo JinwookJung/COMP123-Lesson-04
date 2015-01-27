f6e957f48b50c85177662926d10900c5d26cb3d5
Jinwook Jung <dddrdf@gmail.com>
2015-01-27 13:19:02 -0500

    added Console

---
+++ COMP123 - Lesson - Part02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123___Lesson___Part02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
